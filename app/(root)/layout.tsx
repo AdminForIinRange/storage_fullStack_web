92e8251db6873c2d851014736aad822d88e3dbaa
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-19 21:42:49 +1030

    setting up teh basics for index aka "/" route

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+// import { getCurrentUser } from "@/lib/actions/user.actions";
+// import { redirect } from "next/navigation";
+// import { Toaster } from "@/components/ui/toaster";
+
+import "../globals.css";
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="flex h-screen">
+      <Sidebar />
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation />
+        <Header />
+        <div className="main-content">{children}</div>
+      </section>
+      {/* <Toaster /> */}
+    </main>
+  );
+};
+
+export default Layout;
