f59d7dba26ff27db2c306e9be9bb464cc931f852
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-19 15:28:19 +1030

    getting shadcn wokring and getting utlis working too

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+"use client";
 
-type AuthFormProps = "sign-in" | "sign-up"
-function AuthForm({type}: {type: AuthFormProps}) {
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+type AuthFormProps = "sign-in" | "sign-up";
+function AuthForm({ type }: { type: AuthFormProps }) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div>{type}</div>
-  )
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <h1>{type === "sign-in" ? "Sign in" : "Sign up"}</h1>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
 }
 
-export default AuthForm
+export default AuthForm;
